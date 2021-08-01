b6a20a1d02171bcb7f9ef1e477ab070d370a7ab8
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-08-01 21:40:12 +0300

    feat: 14-primes

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -5,12 +5,28 @@
  * Функция должна возвращать массив простых чисел от 2 до n.
  * 
 */
+function isPrime(n) {
+  if (typeof n !== 'number' || !Number.isInteger(n))
+    return console.error("error: integer expected!");
+  if (n <= 1) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
 
 function primes(num) {
-    // Напишите код здесь
+  if (typeof num !== 'number' || !Number.isInteger(num))
+    return console.error("error: integer expected!");
+  if (num <= 1) return [];
+  const res = [];
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) res.push(i);
+  }
+  return res;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
-
+//console.log(primes('42')); // error: integer expected!
 console.log(primes(6)); // [2, 3, 5]
 console.log(primes(17)); // [2, 3, 5, 7, 11, 13, 17]
