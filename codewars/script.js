e96ba5639ab8f3566e1717ee60231644c8c60f2b
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-08-13 02:45:08 +0300

    feat: задачи от 13.08.21

--- codewars/script.js
+++ codewars/script.js
@@ -101,19 +101,69 @@ function multiply(a, b) {
   return (BigInt(a) * BigInt(b)).toString();
 }
 
-console.log(multiply("2", "3"), "6");
-console.log(multiply("30", "69"), "2070");
-console.log(multiply("11", "85"), "935");
-console.log(multiply("2", "0"), "0");
-console.log(multiply("0", "30"), "0");
-console.log(multiply("0000001", "3"), "3");
-console.log(multiply("1009", "03"), "3027");
-console.log(multiply("98765", "56894"), "5619135910");
-console.log(multiply("1020303004875647366210", "2774537626200857473632627613"), "2830869077153280552556547081187254342445169156730");
-console.log(multiply("58608473622772837728372827", "7586374672263726736374"), "444625839871840560024489175424316205566214109298");
-console.log(multiply("9007199254740991", "9007199254740991"), "81129638414606663681390495662081");
+// console.log(multiply("2", "3"), "6");
+// console.log(multiply("30", "69"), "2070");
+// console.log(multiply("11", "85"), "935");
+// console.log(multiply("2", "0"), "0");
+// console.log(multiply("0", "30"), "0");
+// console.log(multiply("0000001", "3"), "3");
+// console.log(multiply("1009", "03"), "3027");
+// console.log(multiply("98765", "56894"), "5619135910");
+// console.log(multiply("1020303004875647366210", "2774537626200857473632627613"), "2830869077153280552556547081187254342445169156730");
+// console.log(multiply("58608473622772837728372827", "7586374672263726736374"), "444625839871840560024489175424316205566214109298");
+// console.log(multiply("9007199254740991", "9007199254740991"), "81129638414606663681390495662081");
 
-function divideStrings(a,b) {
-  return [Math.floor(BigInt(a) / BigInt(a)).toString(), (BigInt(a) % BigInt(a)).toString()]; 
+function divideStrings(a, b) {
+  return [Math.floor(BigInt(a) / BigInt(a)).toString(), (BigInt(a) % BigInt(a)).toString()];
 }
 
+function formatDuration(seconds) {
+  if (seconds === 0) return 'now';
+  let years = Math.floor(seconds / (3600 * 24 * 365));
+  let days = Math.floor(seconds % (3600 * 24 * 365) / (3600 * 24));
+  let hours = Math.floor(seconds % (3600 * 24) / 3600);
+  let min = Math.floor(seconds % 3600 / 60);
+  let sec = Math.floor(seconds % 60);
+  let ans = [];
+
+  if (years > 1)
+    ans.push(years + ' years');
+  else {
+    if (years === 1)
+      ans.push('1 year');
+  }
+  if (days > 1)
+    ans.push(days + ' days');
+  else {
+    if (days === 1)
+      ans.push('1 day');
+  }
+  if (hours > 1)
+    ans.push(hours + ' hours');
+  else {
+    if (hours === 1)
+      ans.push('1 hour');
+  }
+  if (min > 1)
+    ans.push(min + ' minutes');
+  else {
+    if (min === 1)
+      ans.push('1 minute');
+  }
+  if (sec > 1)
+    ans.push(sec + ' seconds');
+  else {
+    if (sec === 1)
+      ans.push('1 second');
+  }
+
+  if (ans.length > 1)
+    return `${ans.slice(0, ans.length - 1).join(', ')} and ${ans[ans.length - 1]}`;
+  else return ans[0];
+}
+
+console.log(formatDuration(1), " | 1 second");
+console.log(formatDuration(62), "| 1 minute and 2 seconds");
+console.log(formatDuration(120), "| 2 minutes");
+console.log(formatDuration(3600), "| 1 hour");
+console.log(formatDuration(3662), "| 1 hour, 1 minute and 2 seconds");
