0cbb6e9afbbf54c95bd91cf1e5a92bdb92c8a374
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-08-15 22:28:55 +0300

    feat: задачи от 15.08.21

--- codewars/script.js
+++ codewars/script.js
@@ -273,3 +273,43 @@ function permutations(string) {
 // console.log(permutations('ab').sort(), ['ab', 'ba'].sort());
 // console.log(permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort());
 
+/*
+* Most frequently used words in a text
+* https://www.codewars.com/kata/51e056fe544cf36c410000fb
+*/
+
+function topThreeWords(text) {
+  const separators = [' ', '\\.', ',', ';', ':', '-', '\\!', '\\?', '//',];
+  const dict = {}
+  const words = text.toLowerCase().replace(/\s['"]+\s/g, '').split(new RegExp(separators.join('|'), 'g')).filter(item => item);
+  const res = words.forEach(item => {
+    if (!dict[item])
+      dict[item] = 1;
+    else
+      dict[item] += 1;
+  })
+
+  const sortable = [];
+  for (let word in dict) {
+    sortable.push([word, dict[word]]);
+  }
+
+  sortable.sort((a, b) => b[1] - a[1]);
+  const mostFreq = sortable.slice(0, 3).map(item => item[0]);
+
+  return mostFreq;
+}
+
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"), ['e', 'd', 'a'])
+console.log(topThreeWords("a a c b b"), ['a', 'b', 'c'])
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"), ['e', 'ddd', 'aa'])
+console.log(topThreeWords("  //wont won't won't "), ["won't", "wont"])
+console.log(topThreeWords("  , e   .. "), ["e"])
+console.log(topThreeWords("  ...  "), [])
+console.log(topThreeWords("  '  "), [])
+console.log(topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.`), ['a', 'of', 'on'])
